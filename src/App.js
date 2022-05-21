bdce1e5f9b11445006b2967dacb19b9a1e057d99
Kenneth Jimmy <kenjimmy17@gmail.com>
2022-05-21 19:03:10 +0100

    ♻️  Change class to className

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 function App() {
   return (
     <div className="container">
-      <div class="row">
-        <div class="col-xl-12">
+      <div className="row">
+        <div className="col-xl-12">
           <h1>RSS Feeds</h1>
         </div>
 
-        <div class="row">
-          <div class="col-xl-12">
-            <form class="form">
-              <div class="form-group">{/* <InputField /> */}</div>
-              <div class="form-group">{/* <SubmitBtn /> */}</div>
+        <div className="row">
+          <div className="col-xl-12">
+            <form className="form">
+              <div className="form-group">{/* <InputField /> */}</div>
+              <div className="form-group">{/* <SubmitBtn /> */}</div>
             </form>
           </div>
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-xl-12">
+      <div className="row">
+        <div className="col-xl-12">
           <h1>Articles</h1>
 
           {/* <Article /> */}
