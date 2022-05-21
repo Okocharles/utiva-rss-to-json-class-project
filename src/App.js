0360802df897f993c46bfd65d7fcae19a60dbb49
Kenneth Jimmy <kenjimmy17@gmail.com>
2022-05-21 18:46:12 +0100

    ✅ Set up basic markup

--- src/App.js
+++ src/App.js
@@ -7,6 +7,23 @@ function App() {
         <div class="col-xl-12">
           <h1>RSS Feeds</h1>
         </div>
+
+        <div class="row">
+          <div class="col-xl-12">
+            <form class="form">
+              <div class="form-group">{/* <InputField /> */}</div>
+              <div class="form-group">{/* <SubmitBtn /> */}</div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xl-12">
+          <h1>Articles</h1>
+
+          {/* <Article /> */}
+        </div>
       </div>
     </div>
   );
